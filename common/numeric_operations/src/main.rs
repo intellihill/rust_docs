5deb445349869679b73a1881d35bee93e606de67
intelli.hill <intelli.hill@outlook.com>
2024-07-25 07:05:20 +0900

    rust: type

--- common/numeric_operations/src/main.rs
+++ common/numeric_operations/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Resuls in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // boolean
+    let t = true;
+
+    let f: bool = false;
+
+    // character
+    let c = 'z';
+    let z: char = 'Z' // with explicit type annotation
+    let heart_eyed_cat = '🐱';
 }
